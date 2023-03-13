2250639f9be93919320d050c1141ea0d93941021
tracer-ics2017 <tracer@njuics.org>
2023-03-13 17:08:07 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  17:08:07 up 20 min,  1 user,  load average: 0.12, 0.11, 0.11 97d5041d582b51cabb0924f4c3f56c59826864d

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -4,7 +4,9 @@
 #include "common.h"
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
+
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
+
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
 /* TODO: Re-organize the `CPU_state' structure to match the register
@@ -16,17 +18,19 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
 typedef struct {
   union {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+    union {
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
 
-  /* Do NOT change the order of the GPRs' definitions. */
+    /* Do NOT change the order of the GPRs' definitions. */
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+    /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  };
 
   vaddr_t eip;
 
@@ -50,10 +54,10 @@ extern const char* regsb[];
 static inline const char* reg_name(int index, int width) {
   assert(index >= 0 && index < 8);
   switch (width) {
-    case 4: return regsl[index];
-    case 1: return regsb[index];
-    case 2: return regsw[index];
-    default: assert(0);
+    case 4 : return regsl[index];
+    case 1 : return regsb[index];
+    case 2 : return regsw[index];
+    default : assert(0);
   }
 }
 
