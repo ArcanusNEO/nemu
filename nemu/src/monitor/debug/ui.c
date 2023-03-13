055c6fcb7b7c22f8eb30359f07b2f5e0bec0df0c
tracer-ics2017 <tracer@njuics.org>
2023-03-13 23:40:45 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  23:40:45 up 49 min,  1 user,  load average: 0.03, 0.10, 0.05 b38d46f74df173b6395d4ffb68966892d565fc0b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -1,17 +1,20 @@
-#include "monitor/monitor.h"
 #include "monitor/expr.h"
+#include "monitor/monitor.h"
 #include "monitor/watchpoint.h"
 #include "nemu.h"
 
-#include <stdlib.h>
-#include <readline/readline.h>
 #include <readline/history.h>
+#include <readline/readline.h>
+#include <stdlib.h>
 
 void cpu_exec(uint64_t);
 
-/* We use the `readline' library to provide more flexibility to read from stdin. */
+/* We use the `readline' library to provide more flexibility to read from stdin.
+
+Call readline and add_history, return the line read
+*/
 char* rl_gets() {
-  static char *line_read = NULL;
+  static char* line_read = NULL;
 
   if (line_read) {
     free(line_read);
@@ -27,45 +30,51 @@ char* rl_gets() {
   return line_read;
 }
 
-static int cmd_c(char *args) {
+static int cmd_c(char* args) {
   cpu_exec(-1);
   return 0;
 }
 
-static int cmd_q(char *args) {
+static int cmd_q(char* args) {
   return -1;
 }
 
-static int cmd_help(char *args);
+static int cmd_help(char* args);
+
+static int cmd_si(char* args);
 
 static struct {
-  char *name;
-  char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display informations about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
+  char* name;
+  char* description;
+  int (*handler)(char*);
+} cmd_table[] = {
+  {"help",      "Display informations about all supported commands", cmd_help          },
+  {   "c",                            "Continue the execution of the program",    cmd_c},
+  {   "q",                                                        "Exit NEMU",    cmd_q},
 
-  /* TODO: Add more commands */
-
+ /* TODO: Add more commands */
+  {  "si",
+   "Let the program be executed after N instructions is executed single step",   cmd_si},
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
-static int cmd_help(char *args) {
+static int cmd_si(char* args) {
+  return 0;
+}
+
+static int cmd_help(char* args) {
   /* extract the first argument */
-  char *arg = strtok(NULL, " ");
+  char* arg = strtok(NULL, " ");
   int i;
 
   if (arg == NULL) {
     /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
+    for (i = 0; i < NR_CMD; i++) {
       printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
     }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
+  } else {
+    for (i = 0; i < NR_CMD; i++) {
       if (strcmp(arg, cmd_table[i].name) == 0) {
         printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
         return 0;
@@ -83,17 +92,19 @@ void ui_mainloop(int is_batch_mode) {
   }
 
   while (1) {
-    char *str = rl_gets();
-    char *str_end = str + strlen(str);
+    char* str = rl_gets();
+    char* str_end = str + strlen(str);
 
     /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
+    char* cmd = strtok(str, " ");
+    if (cmd == NULL) {
+      continue;
+    }
 
     /* treat the remaining string as the arguments,
      * which may need further parsing
      */
-    char *args = cmd + strlen(cmd) + 1;
+    char* args = cmd + strlen(cmd) + 1;
     if (args >= str_end) {
       args = NULL;
     }
@@ -104,13 +115,17 @@ void ui_mainloop(int is_batch_mode) {
 #endif
 
     int i;
-    for (i = 0; i < NR_CMD; i ++) {
+    for (i = 0; i < NR_CMD; i++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
+        if (cmd_table[i].handler(args) < 0) {
+          return;
+        }
         break;
       }
     }
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    if (i == NR_CMD) {
+      printf("Unknown command '%s'\n", cmd);
+    }
   }
 }
