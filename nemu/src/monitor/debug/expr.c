5869a252cbd707f165599dd5745923bcf3ecbb55
tracer-ics2017 <tracer@njuics.org>
2023-03-15 14:10:35 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  14:10:35 up 11 min,  1 user,  load average: 0.09, 0.13, 0.08 eba9dc9e5a13662a2e77dbe9512a127cf5f98ab7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -343,14 +343,14 @@ uint32_t expr(char* e, bool* success) {
           case '*' : ans = x * y; break;
           case '/' :
             if (y == 0) {
-              Log("Invalid division expression: %lld ÷ 0", x);
+              Log("Invalid division expression: %" PRId64 " ÷ 0", x);
               goto L_EXPR_RELEASE;
             }
             ans = x / y;
             break;
           case '%' :
             if (y == 0) {
-              Log("Invalid division expression: %lld mod 0", x);
+              Log("Invalid division expression: %" PRId64 " mod 0", x);
               goto L_EXPR_RELEASE;
             }
             ans = x % y;
