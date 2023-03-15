c4fded7678ffc0f26440f8b1f11889effe8ee4f0
tracer-ics2017 <tracer@njuics.org>
2023-03-15 18:29:57 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  18:29:57 up 28 min,  1 user,  load average: 0.04, 0.20, 0.15 d0a2c11d27fc611b810c72b42c4f40afe252ba2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -277,15 +277,15 @@ static int64_t readvar(Token* tk) {
     case TK_OCT : sscanf(tk->str, "%" SCNo64, &ans); break;
     case TK_REG :
       s = tk->str + 1;
+      void* p;
       if (s[0] <= ' ' || s[1] <= ' ' ||
         (!REG_32(s) && !REG_16(s) && !REG_8(s)) ||
-        regp[REG_NAME_HASH(s)] == NULL) {
-        
+        ((p = regp[REG_NAME_HASH(s)]) == NULL))
         break;
-      }
-      if (REG_32(s)) ans = (int64_t) (*(uint32_t*) regp[REG_NAME_HASH(s)]);
-      else if (REG_16(s)) ans = (int64_t) (*(uint16_t*) regp[REG_NAME_HASH(s)]);
-      else if (REG_8(s)) ans = (int64_t) (*(uint8_t*) regp[REG_NAME_HASH(s)]);
+
+      if (REG_32(s)) ans = (int64_t) (*(uint32_t*) p);
+      else if (REG_16(s)) ans = (int64_t) (*(uint16_t*) p);
+      else if (REG_8(s)) ans = (int64_t) (*(uint8_t*) p);
       break;
     default : break;
   }
