c791acee113f57906db3d031ccbf48fe1c0da5dd
tracer-ics2017 <tracer@njuics.org>
2023-03-17 00:50:06 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  00:50:06 up 24 min,  1 user,  load average: 0.39, 0.42, 0.23 4aaaad39bd67b71a56980a4091c5aea9a9328cc4

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,17 +1,16 @@
-#include "list.h"
 #include "neoc.h"
+
+#include "list.h"
 
 int init_monitor(int, char*[], int, void*);
 void ui_mainloop(int);
 
-int main(int argc, char* argv[]) {
+signed main(int argc, char* argv[]) {
   /* Initialize the monitor. */
   smart_def(list, smart_wp_pool);
   void* initv[] = {smart_wp_pool};
-  int is_batch_mode = init_monitor(argc, argv, 1, initv);
+  int is_batch_mode = init_monitor(argc, argv, lengthof(initv), initv);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
-
-  return 0;
 }
