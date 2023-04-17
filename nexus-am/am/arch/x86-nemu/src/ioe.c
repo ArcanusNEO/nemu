d167c2abed386392fd5d35d3e9d9ed6296037d1f
tracer-ics2017 <tracer@njuics.org>
2023-04-18 00:25:01 +0800

    >  run 2013280 lucas Linux lucas-laptop 6.2.11-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:00 +0000 x86_64 GNU/Linux  00:25:01 up  2:12,  1 user,  load average: 0.68, 1.00, 1.23 5f2fa57d1558605640156a0e88cd88f18f5d0af0

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,7 +1,10 @@
 #include <am.h>
 #include <x86.h>
 
-#define RTC_PORT 0x48   // Note that this is not standard
+#define RTC_PORT   0x48  // Note that this is not standard
+#define KEY_REG    0x60
+#define KEY_STATUS 0x64
+
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -12,25 +15,26 @@ unsigned long _uptime() {
   return inl(RTC_PORT) - boot_time;
 }
 
-uint32_t* const fb = (uint32_t *)0x40000;
+uint32_t* const fb = (uint32_t*) 0x40000;
 
 _Screen _screen = {
-  .width  = 400,
+  .width = 400,
   .height = 300,
 };
 
-extern void* memcpy(void *, const void *, int);
+extern void* memcpy(void*, const void*, int);
 
-void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
+void _draw_rect(const uint32_t* pixels, int x, int y, int w, int h) {
   int i;
   for (i = 0; i < _screen.width * _screen.height; i++) {
     fb[i] = i;
   }
 }
 
-void _draw_sync() {
-}
+void _draw_sync() { }
 
 int _read_key() {
+  if (inb(KEY_STATUS)) return inl(KEY_REG);
+
   return _KEY_NONE;
 }
