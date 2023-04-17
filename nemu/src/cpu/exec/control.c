85939283641f52002d99e23ac22593c310fb3765
tracer-ics2017 <tracer@njuics.org>
2023-04-17 14:27:06 +0800

    >  compile 2013280 lucas Linux lucas-laptop 6.2.11-zen1-1-zen #1 ZEN SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:00 +0000 x86_64 GNU/Linux  14:27:06 up 32 min,  1 user,  load average: 0.92, 0.76, 0.74 c929a977d08087ecb1e1005d1010802830359417

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,10 +25,8 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  // rtl_push(eip);
+  rtl_push(eip);
   decoding.is_jmp = 1;
-  t0 = *eip;
-  rtl_push(&t0);
 
   print_asm("call %x", decoding.jmp_eip);
 }
