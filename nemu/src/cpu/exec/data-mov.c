79dc30b79cdf93829851d534e435ee5fdd6422dc
tracer-ics2017 <tracer@njuics.org>
2023-05-16 21:37:34 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  21:37:34 up  1:44,  1 user,  load average: 0.00, 0.01, 0.05 13e2bbbecf6dcab489bdfd166c70148f3dd0736e

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -27,13 +27,27 @@ make_EHelper(pop) {
 }
 
 make_EHelper(pusha) {
-  TODO();
+  rtl_push(&cpu.eax);
+  rtl_push(&cpu.ecx);
+  rtl_push(&cpu.edx);
+  rtl_push(&cpu.ebx);
+  rtl_push(&cpu.esp);
+  rtl_push(&cpu.ebp);
+  rtl_push(&cpu.esi);
+  rtl_push(&cpu.edi);
 
   print_asm("pusha");
 }
 
 make_EHelper(popa) {
-  TODO();
+  rtl_pop(&cpu.edi);
+  rtl_pop(&cpu.esi);
+  rtl_pop(&cpu.ebp);
+  rtl_pop(&t0);  // throw esp
+  rtl_pop(&cpu.ebx);
+  rtl_pop(&cpu.edx);
+  rtl_pop(&cpu.ecx);
+  rtl_pop(&cpu.eax);
 
   print_asm("popa");
 }
@@ -49,7 +63,7 @@ make_EHelper(cltd) {
   rtlreg_t msb;
   rtl_msb(&msb, &cpu.eax, 4);
   rtl_neg(&msb);
-  
+
   if (decoding.is_operand_size_16) reg_w(R_DX) = msb;
   else reg_l(R_EDX) = msb;
 
