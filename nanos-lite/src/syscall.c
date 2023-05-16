b0a4e7077bc08ef1d9973b8a60e17e0ba72cd32e
tracer-ics2017 <tracer@njuics.org>
2023-05-16 23:09:27 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  23:09:27 up  3:16,  1 user,  load average: 0.00, 0.06, 0.05 be726ba8b8950bad2ef458e232aa6d06c6bc61af

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,6 +8,8 @@ _RegSet* do_syscall(_RegSet* r) {
   switch (a[0]) {
     case SYS_none : SYSCALL_ARG1(r) = 1; break;
     case SYS_exit : _halt(a[1]); break;
+    case SYS_brk : SYSCALL_ARG1(r) = 0; break;
+    // case SYS_write : SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]); break;
     default : panic("Unhandled syscall ID = %d", a[0]);
   }
 
--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -1,9 +1,9 @@
-#include <unistd.h>
+#include <assert.h>
 #include <stdint.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <assert.h>
 #include <time.h>
+#include <unistd.h>
 #include "syscall.h"
 
 // TODO: discuss with syscall interface
@@ -11,9 +11,9 @@
 
 // FIXME: this is temporary
 
-int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2){
+int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2) {
   int ret = -1;
-  asm volatile("int $0x80": "=a"(ret): "a"(type), "b"(a0), "c"(a1), "d"(a2));
+  asm volatile("int $0x80" : "=a"(ret) : "a"(type), "b"(a0), "c"(a1), "d"(a2));
   return ret;
 }
 
@@ -21,19 +21,27 @@ void _exit(int status) {
   _syscall_(SYS_exit, status, 0, 0);
 }
 
-int _open(const char *path, int flags, mode_t mode) {
+int _open(const char* path, int flags, mode_t mode) {
   _exit(SYS_open);
 }
 
-int _write(int fd, void *buf, size_t count){
-  _exit(SYS_write);
+int _write(int fd, void* buf, size_t count) {
+  _syscall_(SYS_write, fd, (uintptr_t) buf, count);
 }
 
-void *_sbrk(intptr_t increment){
-  return (void *)-1;
+extern char _end;
+static intptr_t brk = &_end;
+
+void* _sbrk(intptr_t increment) {
+  intptr_t prev_brk = brk;
+  if (_syscall_(SYS_brk, brk + increment, 0, 0) == 0) {
+    brk += increment;
+    return (void*) prev_brk;
+  }
+  return (void*) -1;
 }
 
-int _read(int fd, void *buf, size_t count) {
+int _read(int fd, void* buf, size_t count) {
   _exit(SYS_read);
 }
 
@@ -49,16 +57,16 @@ off_t _lseek(int fd, off_t offset, int whence) {
 // But to pass linking, they are defined as dummy functions
 
 // not implement but used
-int _fstat(int fd, struct stat *buf) {
+int _fstat(int fd, struct stat* buf) {
   return 0;
 }
 
-int execve(const char *fname, char * const argv[], char *const envp[]) {
+int execve(const char* fname, char* const argv[], char* const envp[]) {
   assert(0);
   return -1;
 }
 
-int _execve(const char *fname, char * const argv[], char *const envp[]) {
+int _execve(const char* fname, char* const argv[], char* const envp[]) {
   return execve(fname, argv, envp);
 }
 
@@ -72,14 +80,14 @@ pid_t _getpid() {
   return 1;
 }
 
-char **environ;
+char** environ;
 
-time_t time(time_t *tloc) {
+time_t time(time_t* tloc) {
   assert(0);
   return 0;
 }
 
-int signal(int num, void *handler) {
+int signal(int num, void* handler) {
   assert(0);
   return -1;
 }
@@ -89,34 +97,34 @@ pid_t _fork() {
   return -1;
 }
 
-int _link(const char *d, const char *n) {
+int _link(const char* d, const char* n) {
   assert(0);
   return -1;
 }
 
-int _unlink(const char *n) {
+int _unlink(const char* n) {
   assert(0);
   return -1;
 }
 
-pid_t _wait(int *status) {
+pid_t _wait(int* status) {
   assert(0);
   return -1;
 }
 
-clock_t _times(void *buf) {
+clock_t _times(void* buf) {
   assert(0);
   return 0;
 }
 
-int _gettimeofday(struct timeval *tv) {
+int _gettimeofday(struct timeval* tv) {
   assert(0);
   tv->tv_sec = 0;
   tv->tv_usec = 0;
   return 0;
 }
 
-int _fcntl(int fd, int cmd, ... ) {
+int _fcntl(int fd, int cmd, ...) {
   assert(0);
   return 0;
 }
