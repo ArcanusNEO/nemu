0405f068852dac69a05f944c1aebe90f72bba8a2
tracer-ics2017 <tracer@njuics.org>
2023-05-17 01:10:38 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  01:10:38 up  5:17,  1 user,  load average: 0.23, 0.34, 0.22 1b84164e3551f88dc5c1bd0e64fb098c2ede1d1

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -21,12 +21,12 @@ enum {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {  "stdin (note that this is not the actual stdin)",   0, 0},
-  {"stdout (note that this is not the actual stdout)",   0, 0},
-  {"stderr (note that this is not the actual stderr)",   0, 0},
-  [FD_FB] = {                                         "/dev/fb",   0, 0},
-  [FD_EVENTS] = {                                     "/dev/events",   0, 0},
-  [FD_DISPINFO] = {                                  "/proc/dispinfo", 128, 0},
+  {  "stdin (note that this is not the actual stdin)",   1, 0, 0},
+  {"stdout (note that this is not the actual stdout)",   1, 0, 0},
+  {"stderr (note that this is not the actual stderr)",   1, 0, 0},
+  [FD_FB] = {                                         "/dev/fb",   1, 0, 0},
+  [FD_EVENTS] = {                                     "/dev/events",   1, 0, 0},
+  [FD_DISPINFO] = {                                  "/proc/dispinfo", 128, 0, 0},
 #include "files.h"
 };
 
@@ -51,7 +51,9 @@ ssize_t fs_read(int fd, void* buf, size_t len) {
 
   Finfo* f = file_table + fd;
   off_t eof = f->disk_offset + f->size;
-  assert(eof >= f->open_offset);
+  if (fd >= FD_NORMAL &&
+    (f->open_offset >= eof || f->open_offset < f->disk_offset))
+    return 0;
 
   size_t rlen = min(len, (size_t) (eof - f->open_offset));
   ramdisk_read(buf, f->open_offset, rlen);
@@ -64,15 +66,19 @@ ssize_t fs_write(int fd, const void* buf, size_t len) {
 
   Finfo* f = file_table + fd;
   off_t eof = f->disk_offset + f->size;
-  assert(eof >= f->open_offset);
+  if (fd >= FD_NORMAL &&
+    (f->open_offset >= eof || f->open_offset < f->disk_offset))
+    return 0;
 
   switch (fd) {
     case FD_STDOUT :
-    case FD_STDERR :;
+    case FD_STDERR :
+      NULL;
       const char* _buf = buf;
       for (size_t i = 0; i < len; ++i) _putc(_buf[i]);
       return len;
-    case FD_NORMAL :;
+    default :
+      NULL;
       size_t rlen = min(len, (size_t) (eof - f->open_offset));
       ramdisk_write(buf, f->open_offset, rlen);
       f->open_offset += rlen;
@@ -82,7 +88,21 @@ ssize_t fs_write(int fd, const void* buf, size_t len) {
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence) {
-  TODO();
+  assert(fd >= 0);
+
+  Finfo* f = file_table + fd;
+
+  switch (whence) {
+    case SEEK_CUR : f->open_offset += offset; break;
+    case SEEK_END :
+      NULL;
+      off_t eof = f->disk_offset + f->size;
+      f->open_offset = eof + offset;
+      break;
+    case SEEK_SET : f->open_offset = offset; break;
+    default : return -1;
+  }
+
   return 0;
 }
 
