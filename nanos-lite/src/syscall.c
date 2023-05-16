cd5589fdf8fdc80b6249baa2bd88c8a9e9fa8235
tracer-ics2017 <tracer@njuics.org>
2023-05-16 22:09:20 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  22:09:20 up  2:16,  1 user,  load average: 0.08, 0.03, 0.03 c04acbdb7ca44424262651614b42c376365cc3ae

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,13 @@
 #include "common.h"
 #include "syscall.h"
 
-_RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
-  a[0] = SYSCALL_ARG1(r);
+_RegSet* do_syscall(_RegSet* r) {
+  uintptr_t a[4] = {
+    SYSCALL_ARG1(r), SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r)};
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    case _EVENT_SYSCALL : do_syscall(r); break;
+    default : panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
