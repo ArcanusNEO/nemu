585faba46273a461faccb8c63f822866c08ccac7
tracer-ics2017 <tracer@njuics.org>
2023-05-16 20:25:59 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  20:25:59 up 32 min,  1 user,  load average: 0.53, 0.22, 0.24 c1a29930cc8e86adbc6236de39372f721f18360a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,7 +4,7 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  // size_t sz = get_ramdisk_size();
-  // ramdisk_read(DEFAULT_ENTRY, 0, sz);
+  size_t sz = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, sz);
   return (uintptr_t) DEFAULT_ENTRY;
 }
