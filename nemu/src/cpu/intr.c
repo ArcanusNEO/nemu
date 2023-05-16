4ba74a2229c1ff70df7ab928fd9ace5880dfc9c5
tracer-ics2017 <tracer@njuics.org>
2023-05-16 21:22:41 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  21:22:41 up  1:29,  1 user,  load average: 0.04, 0.29, 0.17 96dedd0cfd36645e3363f8e1c995f7c16ebbfec6

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,22 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  vaddr_t gate_addr = cpu.idtr.base + 8 * NO;
+
+  t0 = cpu.eflags;
+  rtl_push(&t0);
+
+  t0 = cpu.cs;
+  rtl_push(&t0);
+
+  t0 = ret_addr;
+  rtl_push(&t0);
+
+  uint32_t low = vaddr_read(gate_addr, 4) & 0xffff;
+  uint32_t high = vaddr_read(gate_addr + 4, 4) & 0xffff0000;
+
+  decoding.jmp_eip = high | low;
+  decoding.is_jmp = true;
 }
 
 void dev_raise_intr() { }
