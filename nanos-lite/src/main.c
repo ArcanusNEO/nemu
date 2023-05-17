08a1b2d95690569886229fede752c4ab7bf60fdc
tracer-ics2017 <tracer@njuics.org>
2023-05-18 03:05:38 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  03:05:38 up 26 min,  1 user,  load average: 0.64, 0.86, 0.53 fbe1b33e656f1336937dd611986de19a55c84a8

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/nterm");
+  uint32_t entry = loader(NULL, "/bin/litenes");
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
