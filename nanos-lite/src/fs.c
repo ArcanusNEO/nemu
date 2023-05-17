ad925aecc3aee15f73dae277d5440ea79453e5ae
tracer-ics2017 <tracer@njuics.org>
2023-05-17 16:15:35 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  16:15:35 up  1:12,  1 user,  load average: 0.55, 0.35, 0.20 443fa4ffad8cb84dd5ecbf7f805641dd8e6766de

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -74,8 +74,8 @@ ssize_t fs_read(int fd, void* buf, size_t len) {
     case FD_STDERR : return 0;
     case FD_FB : return io_helper(fb_read);
     case FD_EVENTS : break;
-    case FD_DISPINFO :
-      f->open_offset = 0;
+    case FD_DISPINFO :;
+      // f->open_offset = f->disk_offset;
       // len = f->size;
       ssize_t ret = io_helper(dispinfo_read);
       // f->open_offset = 0;
