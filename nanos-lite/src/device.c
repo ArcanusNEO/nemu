b95a71097b48e038cb2e69f59f4fb415ceecdda6
tracer-ics2017 <tracer@njuics.org>
2023-05-17 19:29:12 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  19:29:12 up  4:26,  1 user,  load average: 0.19, 0.41, 0.30 524004c1ce1fecdb1c53824e817e4ab58faec8a3

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,7 +23,7 @@ size_t events_read(void* buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void* buf, off_t offset, size_t len) {
-  memset(buf, 0, len);
+  // memset(buf, 0, len);
   strncpy(buf, dispinfo + offset, len);
 }
 
