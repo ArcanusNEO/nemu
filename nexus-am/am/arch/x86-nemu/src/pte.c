afeab896508087343045651b7fe93d9ed8b2c101
tracer-ics2017 <tracer@njuics.org>
2023-05-26 17:46:14 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  17:46:14 up  2:43,  1 user,  load average: 0.02, 0.14, 0.12 b871c9f518dc17b3fb723460afbe28a4bc7950b5

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -7,8 +7,9 @@ static PTE kptabs[PMEM_SIZE / PGSIZE] PG_ALIGN;
 static void* (*palloc_f)();
 static void (*pfree_f)(void*);
 
-_Area segments[] = {      // Kernel memory mappings
-  {.start = (void*)0,          .end = (void*)PMEM_SIZE}
+_Area segments[] = {
+  // Kernel memory mappings
+  {.start = (void*) 0, .end = (void*) PMEM_SIZE}
 };
 
 #define NR_KSEG_MAP (sizeof(segments) / sizeof(segments[0]))
@@ -20,24 +21,24 @@ void _pte_init(void* (*palloc)(), void (*pfree)(void*)) {
   int i;
 
   // make all PDEs invalid
-  for (i = 0; i < NR_PDE; i ++) {
+  for (i = 0; i < NR_PDE; i++) {
     kpdirs[i] = 0;
   }
 
-  PTE *ptab = kptabs;
-  for (i = 0; i < NR_KSEG_MAP; i ++) {
-    uint32_t pdir_idx = (uintptr_t)segments[i].start / (PGSIZE * NR_PTE);
-    uint32_t pdir_idx_end = (uintptr_t)segments[i].end / (PGSIZE * NR_PTE);
-    for (; pdir_idx < pdir_idx_end; pdir_idx ++) {
+  PTE* ptab = kptabs;
+  for (i = 0; i < NR_KSEG_MAP; i++) {
+    uint32_t pdir_idx = (uintptr_t) segments[i].start / (PGSIZE * NR_PTE);
+    uint32_t pdir_idx_end = (uintptr_t) segments[i].end / (PGSIZE * NR_PTE);
+    for (; pdir_idx < pdir_idx_end; pdir_idx++) {
       // fill PDE
-      kpdirs[pdir_idx] = (uintptr_t)ptab | PTE_P;
+      kpdirs[pdir_idx] = (uintptr_t) ptab | PTE_P;
 
       // fill PTE
       PTE pte = PGADDR(pdir_idx, 0, 0) | PTE_P;
       PTE pte_end = PGADDR(pdir_idx + 1, 0, 0) | PTE_P;
       for (; pte < pte_end; pte += PGSIZE) {
         *ptab = pte;
-        ptab ++;
+        ptab++;
       }
     }
   }
@@ -46,31 +47,40 @@ void _pte_init(void* (*palloc)(), void (*pfree)(void*)) {
   set_cr0(get_cr0() | CR0_PG);
 }
 
-void _protect(_Protect *p) {
-  PDE *updir = (PDE*)(palloc_f());
+void _protect(_Protect* p) {
+  PDE* updir = (PDE*) (palloc_f());
   p->ptr = updir;
   // map kernel space
-  for (int i = 0; i < NR_PDE; i ++) {
+  for (int i = 0; i < NR_PDE; i++) {
     updir[i] = kpdirs[i];
   }
 
-  p->area.start = (void*)0x8000000;
-  p->area.end = (void*)0xc0000000;
+  p->area.start = (void*) 0x8000000;
+  p->area.end = (void*) 0xc0000000;
 }
 
-void _release(_Protect *p) {
-}
+void _release(_Protect* p) { }
 
-void _switch(_Protect *p) {
+void _switch(_Protect* p) {
   set_cr3(p->ptr);
 }
 
-void _map(_Protect *p, void *va, void *pa) {
+void _map(_Protect* p, void* va, void* pa) {
+  PDE* pgdir = p->ptr;
+  PDE* pde = &pgdir[PDX(va)];
+  PTE* pgtab;
+  if (*pde & PTE_P) pgtab = (PTE*) PTE_ADDR(*pde);
+  else {
+    pgtab = palloc_f();
+    for (size_t i = 0; i < NR_PTE; ++i) pgtab[i] = 0;
+    *pde = PTE_ADDR(pgtab) | PTE_P;
+  }
+  pgtab[PTX(va)] = PTE_ADDR(pa) | PTE_P;
 }
 
-void _unmap(_Protect *p, void *va) {
-}
+void _unmap(_Protect* p, void* va) { }
 
-_RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *const argv[], char *const envp[]) {
+_RegSet* _umake(_Protect* p, _Area ustack, _Area kstack, void* entry,
+  char* const argv[], char* const envp[]) {
   return NULL;
 }
