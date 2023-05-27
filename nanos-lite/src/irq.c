3300d9c534c3cb57c268c6305e8e946e840a1875
tracer-ics2017 <tracer@njuics.org>
2023-05-27 15:34:35 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  15:34:35 up  1:55,  1 user,  load average: 0.09, 0.19, 0.15 c498775d9484d042caf313af685545029af69192

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ _RegSet* schedule(_RegSet* prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL : do_syscall(r); break;
-    case _EVENT_TRAP : schedule(r); break;
+    case _EVENT_TRAP : return schedule(r);
     default : panic("Unhandled event ID = %d", e.event);
   }
 
