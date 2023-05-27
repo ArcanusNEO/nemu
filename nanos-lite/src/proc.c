19f2c4ca52a670f56f4d63aaf3d3882d67c13683
tracer-ics2017 <tracer@njuics.org>
2023-05-27 14:58:53 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  14:58:53 up  1:19,  1 user,  load average: 0.16, 0.08, 0.06 99b55caf8cd7b14619380379e21b5af8c9646df9

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,5 +27,10 @@ void load_prog(const char* filename) {
 }
 
 _RegSet* schedule(_RegSet* prev) {
-  return NULL;
+  current->tf = prev;
+
+  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+
+  _switch(&current->as);
+  return current->tf;
 }
