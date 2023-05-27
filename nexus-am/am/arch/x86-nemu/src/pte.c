e6084ea7272b9d38c6b1a52c64a698b900bb824f
tracer-ics2017 <tracer@njuics.org>
2023-05-27 10:15:04 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  10:15:04 up 50 min,  1 user,  load average: 0.67, 0.29, 0.16 15a634566644c507f12c4cf70ff8332ed88f6ff

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -72,7 +72,7 @@ void _map(_Protect* p, void* va, void* pa) {
   if (*pde & PTE_P) pgtab = (PTE*) PTE_ADDR(*pde);
   else {
     pgtab = palloc_f();
-    for (size_t i = 0; i < NR_PTE; ++i) pgtab[i] = PTE_P;
+    for (size_t i = 0; i < NR_PTE; ++i) pgtab[i] = 0;
     *pde = PTE_ADDR(pgtab) | PTE_P;
   }
   pgtab[PTX(va)] = PTE_ADDR(pa) | PTE_P;
