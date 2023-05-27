a9cec7dff35f91d2c739c5fc7b12453541ae0794
tracer-ics2017 <tracer@njuics.org>
2023-05-27 09:40:26 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  09:40:26 up 15 min,  1 user,  load average: 0.20, 0.15, 0.09 486ad471b6f6b0ef4ce3e4adb3dcf07aabcc3303

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,12 +11,12 @@ uintptr_t loader(_Protect* as, const char* filename) {
   int fd = fs_open(filename, 0, 0);
   size_t f_size = fs_filesz(fd);
 
-  Log("loaded: [%d]%s size: %d", fd, filename, f_size);
+  // Log("loaded: [%d]%s size: %d", fd, filename, f_size);
 
   void* mend = DEFAULT_ENTRY + f_size;
   for (void* va = DEFAULT_ENTRY; va < mend; va += PGSIZE) {
     void* pa = new_page();
-    Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+    // Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
     _map(as, va, pa);
     fs_read(fd, pa, min(mend - va, PGSIZE));
   }
