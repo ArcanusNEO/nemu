34b28cabefd2aa3a2086c0b4f3fbf63fbb5c323c
tracer-ics2017 <tracer@njuics.org>
2023-05-28 14:54:14 +0800

    >  run 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  14:54:14 up 6 min,  1 user,  load average: 0.71, 0.47, 0.19 74d9a8d85a5b511d9afdc02e453f8519d6d3c8ed

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -34,7 +34,7 @@ signed main(void) {
 
   // uint32_t entry = loader(NULL, "/bin/pal");
   // ((void (*)(void))entry)();
-  load_prog("/bin/pal");
+  // load_prog("/bin/pal");
   load_prog("/bin/hello");
 
   _trap();
