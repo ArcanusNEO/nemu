6c013bf78982cf77b34657508e48f71bfc3381fd
tracer-ics2017 <tracer@njuics.org>
2023-06-15 11:31:02 +0800

    >  compile 2013280 lucas Linux lucas-virt 5.4.0-139-generic #156~18.04.1-Ubuntu SMP Wed Jan 25 15:55:58 UTC 2023 i686 athlon i686 GNU/Linux  11:31:02 up 48 min,  1 user,  load average: 0.48, 0.62, 0.54 68cac7a159656d8329153e09657e4593424fbad4

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -201,7 +201,7 @@ opcode_entry opcode_table[512] = {
   /* 0x9c */ IDEXW(E, setcc, 1), IDEXW(E, setcc, 1), IDEXW(E, setcc, 1),
   IDEXW(E, setcc, 1),
   /* 0xa0 */ EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xa4 */ EMPTY, IDEX(Ib_G2E, shld), EMPTY, EMPTY,
+  /* 0xa4 */ IDEX(Ib_G2E, shld), EMPTY, EMPTY, EMPTY,
   /* 0xa8 */ EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xac */ IDEX(Ib_G2E, shrd), EMPTY, EMPTY, IDEX(E2G, imul2),
   /* 0xb0 */ EMPTY, EMPTY, EMPTY, EMPTY,
